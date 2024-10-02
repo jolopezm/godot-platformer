5eca563797cc4fddf40b76ba19683dc14f61c189
Jose Lopez <joselopezmignone7@gmail.com>
2024-10-02 17:44:48 -0300

    se creo el player y el tilemap

---
+++ scenes/player controller/CharacterController.cs
@@ -0,0 +1,58 @@
+using Godot;
+using System;
+
+public partial class CharacterController : CharacterBody2D
+{
+	public float Speed = 300.0f;
+	public float JumpVelocity = -400.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+			if (Input.IsActionJustReleased("jump")) {
+				velocity.Y /= 40;
+			}
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("left", "right", "up", "down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
+
+/*
+extends CharacterBody2D
+
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
+
+*/
